5c19aa98639ed528dfda7d8962c692b737f3bdbd
Brian <brianlebigot@gmail.com>
2022-06-13 01:04:00 +0200

    minor change scrollY

--- menu_user.js
+++ menu_user.js
@@ -1,22 +1,7 @@
-// if ((window).scroll(function(e){ 
-//     const menu_user = document.getElementById("menu_user"); 
-//     const isPositionFixed = menu_user.css('position') == 'fixed';
-//     if ($(this).scrollTop() > 300 && !isPositionFixed){ 
-//       menu_user.setAttribute("position", "fixed");
-//       menu_user.setAttribute("top", "0px");
-//     }
-//     if ($(this).scrollTop() < 300 && isPositionFixed){
-//       menu_user.setAttribute("position", "static");
-//       menu_user.setAttribute("top", "0px");
-//     } 
-//   }));
-
-console.log("hello")
 window.addEventListener('scroll', function() {
-    console.log("launch")
     const menu_user = document.getElementById("menu_user");
     if (window. screen.width > 768) {
-        if (window.scrollY > 300) {
+        if (window.scrollY > 330) {
             menu_user.classList.add("fixed-top");
         } else {
             menu_user.classList.remove("fixed-top");
